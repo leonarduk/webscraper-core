28d718b8569633b1c8da7d81f4da722d30c43c18
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-01-24 22:49:12 +0000

    initial

---
+++ src/main/java/com/leonarduk/core/config/Config.java
@@ -0,0 +1,82 @@
+package com.leonarduk.core.config;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * 
+ * 
+ *
+ * @author Stephen Leonard
+ * @since 24 Jan 2015
+ *
+ * @version $Author:: $: Author of last commit
+ * @version $Rev:: $: Revision of last commit
+ * @version $Date:: $: Date of last commit
+ *
+ */
+public class Config {
+
+	/**
+	 * 
+	 */
+	private Properties props;
+
+	/**
+	 * 
+	 * @throws IOException
+	 */
+	public Config(String propFileName) throws IOException {
+		this.props = new Properties();
+
+		InputStream inputStream = getClass().getClassLoader()
+				.getResourceAsStream(propFileName);
+
+		if (inputStream != null) {
+			props.load(inputStream);
+		} else {
+			throw new FileNotFoundException("property file '" + propFileName
+					+ "' not found in the classpath");
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param fieldName
+	 * @return
+	 */
+	public String getProperty(String fieldName) {
+		return this.props.getProperty(fieldName);
+	}
+
+	/**
+	 * 
+	 * @param fieldName
+	 * @return
+	 * @throws NumberFormatException
+	 */
+	public Integer getIntegerProperty(String fieldName)
+			throws NumberFormatException {
+		String value = getProperty(fieldName);
+		if (null == value)
+			return null;
+		return Integer.valueOf(value);
+	}
+
+	/**
+	 * 
+	 * @param fieldName
+	 * @return
+	 * @throws NumberFormatException
+	 */
+	public Double getDoubleProperty(String fieldName)
+			throws NumberFormatException {
+		String value = getProperty(fieldName);
+		if (null == value)
+			return null;
+		return Double.valueOf(value);
+	}
+}
---
+++ src/test/java/com/leonarduk/core/config/ConfigTest.java
@@ -0,0 +1,67 @@
+package com.leonarduk.core.config;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConfigTest {
+
+	private Config config;
+
+	@Before
+	public void setUp() throws Exception {
+		config = new Config("test.properties");
+	}
+
+	@Test(expected = FileNotFoundException.class)
+	public final void testConfig() throws IOException {
+		new Config("missing.test.properties");
+	}
+
+	@Test
+	public final void testGetProperty() {
+		assertEquals("eggs and bacon", config.getProperty("test.string"));
+	}
+
+	@Test
+	public final void testGetIntegerProperty() {
+		final int expected = 12;
+		assertEquals(expected, config.getIntegerProperty("test.int"), 0);
+	}
+
+	@Test
+	public final void testGetDoubleProperty() {
+		final double expected = 12.34;
+		assertEquals(expected, config.getDoubleProperty("test.double"), 0);
+	}
+
+	@Test
+	public final void testGetMissingProperty() {
+		assertNull(config.getProperty("test.string.missing"));
+	}
+
+	@Test
+	public final void testGetMissingIntegerProperty() {
+		assertNull(config.getIntegerProperty("test.int.missing"));
+	}
+
+	@Test
+	public final void testGetMissingDoubleProperty() {
+		assertNull(config.getDoubleProperty("test.double.missing"));
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public final void testGetInvalidIntegerProperty() {
+		assertNull(config.getIntegerProperty("test.string"));
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public final void testGetInvalidDoubleProperty() {
+		assertNull(config.getDoubleProperty("test.string"));
+	}
+}
