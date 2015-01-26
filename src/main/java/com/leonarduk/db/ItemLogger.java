0d55b77a4d55c9c162e5cc0b282322d69d16621d
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-01-26 22:21:33 +0000

    remove dependency

---
+++ src/main/java/com/leonarduk/db/ItemLogger.java
@@ -0,0 +1,26 @@
+package com.leonarduk.db;
+
+
+public class ItemLogger {
+
+	public void handleDebugException(String string, Exception e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handleException(String string, Exception e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void logDebug(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void logMessage(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
