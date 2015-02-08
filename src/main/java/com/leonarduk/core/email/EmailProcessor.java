e1e4f63b8752e9d3e20b1cfdf2f89c160f5bedcf
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-02-08 21:17:53 +0000

    test svn

--- src/main/java/com/leonarduk/core/email/EmailProcessor.java
+++ src/main/java/com/leonarduk/core/email/EmailProcessor.java
@@ -7,9 +7,9 @@ package com.leonarduk.core.email;
  * The Interface EmailProcessor.
  *
  * @author Stephen Leonard
- * @version $Author: $: Author of last commit
+ * @version $Author$: Author of last commit
  * @version $Rev: $: Revision of last commit
- * @version $Date$: Date of last commit
+ * @version $Date: 2015-02-08 21:17:01 +0000 (Sun, 08 Feb 2015) $: Date of last commit
  * @since 2 Feb 2015
  */
 public interface EmailProcessor {
