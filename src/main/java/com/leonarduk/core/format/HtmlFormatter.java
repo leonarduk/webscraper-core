a2300abd302130ed53e1efdfcf332f9e3132a977
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-05-07 16:03:06 +0000

    add formatBody to split text into new lines

--- src/main/java/com/leonarduk/core/format/HtmlFormatter.java
+++ src/main/java/com/leonarduk/core/format/HtmlFormatter.java
@@ -3,6 +3,7 @@
  */
 package com.leonarduk.core.format;
 
+
 /**
  * The Class HtmlFormatter.
  *
@@ -14,70 +15,76 @@ package com.leonarduk.core.format;
  */
 public class HtmlFormatter implements Formatter {
 
-	/**
-	 * Creates the node.
-	 *
-	 * @param value
-	 *            the value
-	 * @param node
-	 *            the node
-	 * @return the string
-	 */
-	private String createNode(final String value, final String node) {
-		return new StringBuilder("<").append(node).append(">").append(value).append("</")
-				.append(node).append(">").toString();
-	}
+    /**
+     * Creates the node.
+     *
+     * @param value
+     *            the value
+     * @param node
+     *            the node
+     * @return the string
+     */
+    private String createNode(final String value, final String node) {
+        return new StringBuilder("<").append(node).append(">").append(value)
+                .append("</").append(node).append(">").toString();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see com.leonarduk.itemfinder.format.Formatter#formatHeader(java.lang.String)
-	 */
-	@Override
-	public final String formatHeader(final String header) {
-		return this.createNode(header, "h1");
-	}
+    /*
+     * (non-Javadoc)
+     * @see
+     * com.leonarduk.itemfinder.format.Formatter#formatHeader(java.lang.String)
+     */
+    @Override
+    public final String formatHeader(final String header) {
+        return this.createNode(header, "h1");
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see com.leonarduk.itemfinder.format.Formatter#formatLink(java.lang.String, java.lang.String)
-	 */
-	@Override
-	public final String formatLink(final String link, final String name) {
-		final StringBuilder linkBuilder = new StringBuilder("<a href=\"" + link + "\">");
-		linkBuilder.append(name);
-		linkBuilder.append("</a>");
-		return linkBuilder.toString();
-	}
+    /*
+     * (non-Javadoc)
+     * @see
+     * com.leonarduk.itemfinder.format.Formatter#formatLink(java.lang.String,
+     * java.lang.String)
+     */
+    @Override
+    public final String formatLink(final String link, final String name) {
+        final StringBuilder linkBuilder =
+                new StringBuilder("<a href=\"" + link + "\">");
+        linkBuilder.append(name);
+        linkBuilder.append("</a>");
+        return linkBuilder.toString();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see com.leonarduk.itemfinder.format.Formatter#formatSubHeader(java.lang.String)
-	 */
-	@Override
-	public final String formatSubHeader(final String header) {
-		return this.createNode(header, "h2");
-	}
+    /*
+     * (non-Javadoc)
+     * @see
+     * com.leonarduk.itemfinder.format.Formatter#formatSubHeader(java.lang.String
+     * )
+     */
+    @Override
+    public final String formatSubHeader(final String header) {
+        return this.createNode(header, "h2");
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see com.leonarduk.itemfinder.format.Formatter#getNewLine()
-	 */
-	@Override
-	public final String getNewLine() {
-		return "<br/>";
-	}
+    /*
+     * (non-Javadoc)
+     * @see com.leonarduk.itemfinder.format.Formatter#getNewLine()
+     */
+    @Override
+    public final String getNewLine() {
+        return "<br/>";
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see com.leonarduk.itemfinder.format.Formatter#getNewSection()
-	 */
-	@Override
-	public final String getNewSection() {
-		return "<hr/>";
-	}
+    /*
+     * (non-Javadoc)
+     * @see com.leonarduk.itemfinder.format.Formatter#getNewSection()
+     */
+    @Override
+    public final String getNewSection() {
+        return "<hr/>";
+    }
+
+    @Override
+    public String formatBody(String text) {
+        return text.replaceAll("\n", getNewLine());
+    }
 }
