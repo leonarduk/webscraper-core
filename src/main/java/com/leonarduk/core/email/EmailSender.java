bb8666e4631a06c332d854fe4c120f0e9ee0f0e4
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-12-11 23:24:41 +0000

    [[#110004536]] work on upload bugs

--- src/main/java/com/leonarduk/core/email/EmailSender.java
+++ src/main/java/com/leonarduk/core/email/EmailSender.java
@@ -4,6 +4,7 @@
 package com.leonarduk.core.email;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.Date;
 
 import javax.mail.Message;
@@ -20,102 +21,86 @@ import org.apache.log4j.Logger;
  */
 public class EmailSender {
 
-    /** The logger. */
-    private static Logger logger = Logger.getLogger(EmailSender.class);
+	/** The logger. */
+	private static Logger logger = Logger.getLogger(EmailSender.class);
 
-    /**
-     * Creates the message.
-     *
-     * @param fromEmail
-     *            the from email
-     * @param fromName
-     *            the from name
-     * @param subject
-     *            the subject
-     * @param msgBody
-     *            the msg body
-     * @param html
-     *            the html
-     * @param session
-     *            the session
-     * @param to
-     *            the to
-     * @return the message
-     * @throws MessagingException
-     *             the messaging exception
-     * @throws UnsupportedEncodingException
-     *             the unsupported encoding exception
-     */
-    public final Message createMessage(
-            final String fromEmail,
-            final String fromName,
-            final String subject,
-            final String msgBody,
-            final boolean html,
-            final EmailSession session,
-            final String... to)
-            throws MessagingException,
-            UnsupportedEncodingException {
-        final Message msg = new MimeMessage(session.getSession());
-        final InternetAddress[] addressTo = new InternetAddress[to.length];
-        for (int i = 0; i < to.length; i++) {
-            addressTo[i] = new InternetAddress(to[i]);
-        }
-        msg.setRecipients(RecipientType.TO, addressTo);
+	/**
+	 * Creates the message.
+	 *
+	 * @param fromEmail
+	 *            the from email
+	 * @param fromName
+	 *            the from name
+	 * @param subject
+	 *            the subject
+	 * @param msgBody
+	 *            the msg body
+	 * @param html
+	 *            the html
+	 * @param session
+	 *            the session
+	 * @param to
+	 *            the to
+	 * @return the message
+	 * @throws MessagingException
+	 *             the messaging exception
+	 * @throws UnsupportedEncodingException
+	 *             the unsupported encoding exception
+	 */
+	public final Message createMessage(final String fromEmail, final String fromName, final String subject,
+			final String msgBody, final boolean html, final EmailSession session, final String... to)
+					throws MessagingException, UnsupportedEncodingException {
+		final Message msg = new MimeMessage(session.getSession());
+		final InternetAddress[] addressTo = new InternetAddress[to.length];
+		for (int i = 0; i < to.length; i++) {
+			addressTo[i] = new InternetAddress(to[i]);
+		}
+		msg.setRecipients(RecipientType.TO, addressTo);
 
-        msg.setFrom(new InternetAddress(fromEmail, fromName));
-        msg.setSentDate(new Date());
-        msg.setSubject(subject);
-        if (html) {
-            msg.setContent(msgBody, "text/html");
-        }
-        else {
-            msg.setText(msgBody);
-        }
-        return msg;
-    }
+		msg.setFrom(new InternetAddress(fromEmail, fromName));
+		msg.setSentDate(new Date());
+		msg.setSubject(subject);
+		if (html) {
+			msg.setContent(msgBody, "text/html");
+		} else {
+			msg.setText(msgBody);
+		}
+		return msg;
+	}
 
-    /**
-     * Send message.
-     *
-     * @param fromEmail
-     *            the from email
-     * @param fromName
-     *            the from name
-     * @param subject
-     *            the subject
-     * @param msgBody
-     *            the msg body
-     * @param html
-     *            the html
-     * @param session
-     *            the session
-     * @param to
-     *            the to
-     * @throws EmailException
-     *             the email exception
-     */
-    public final void sendMessage(
-            final String fromEmail,
-            final String fromName,
-            final String subject,
-            final String msgBody,
-            final boolean html,
-            final EmailSession session,
-            final String... to) throws EmailException {
-        try {
-            EmailSender.logger.info("sendMessage: from " + fromEmail + " to "
-                                    + to);
-            final Message msg =
-                    this.createMessage(fromEmail, fromName, subject, msgBody,
-                            html, session, to);
+	/**
+	 * Send message.
+	 *
+	 * @param fromEmail
+	 *            the from email
+	 * @param fromName
+	 *            the from name
+	 * @param subject
+	 *            the subject
+	 * @param msgBody
+	 *            the msg body
+	 * @param html
+	 *            the html
+	 * @param session
+	 *            the session
+	 * @param to
+	 *            the to
+	 * @throws EmailException
+	 *             the email exception
+	 */
+	public final void sendMessage(final String fromEmail, final String fromName, final String subject,
+			final String msgBody, final boolean html, final EmailSession session, final String... to)
+					throws EmailException {
+		try {
+			EmailSender.logger.info("sendMessage: from " + fromEmail + " to " + Arrays.asList(to));
+			EmailSender.logger.debug("Text\n" + msgBody);
+			final Message msg = this.createMessage(fromEmail, fromName, subject, msgBody, html, session, to);
 
-            Transport.send(msg);
+			Transport.send(msg);
 
-        }
-        catch (MessagingException | UnsupportedEncodingException e) {
-            throw new EmailException("Failed to send message", e);
-        }
-    }
+		} catch (MessagingException | UnsupportedEncodingException e) {
+			throw new EmailException("Failed to send message", e);
+		}
+	}
 
 }
--- src/main/java/com/leonarduk/web/BaseSeleniumPage.java
+++ src/main/java/com/leonarduk/web/BaseSeleniumPage.java
@@ -22,174 +22,170 @@ import org.openqa.selenium.support.ui.LoadableComponent;
  *          commit
  * @since 18 Feb 2015
  */
-public abstract class BaseSeleniumPage extends
-        LoadableComponent<BaseSeleniumPage> {
-    /** The web driver. */
-    private final WebDriver webdriver;
+public abstract class BaseSeleniumPage extends LoadableComponent<BaseSeleniumPage> {
+	/** The web driver. */
+	private final WebDriver webdriver;
 
-    private final String expectedUrl;
+	private final String expectedUrl;
 
-    private boolean acceptNextAlert = true;
+	private boolean acceptNextAlert = true;
 
-    /** The Constant ONE_SECOND_IN_MS. */
-    public static final int ONE_SECOND_IN_MS = 1000;
+	/** The Constant ONE_SECOND_IN_MS. */
+	public static final int ONE_SECOND_IN_MS = 1000;
 
-    /** The Constant LOGGER. */
-    static final Logger LOGGER = Logger.getLogger(BaseSeleniumPage.class);
+	/** The Constant LOGGER. */
+	static final Logger LOGGER = Logger.getLogger(BaseSeleniumPage.class);
 
-    /**
-     * Instantiates a new base selenium page.
-     *
-     * @param webDriver
-     *            the web driver
-     * @param expectedUrl
-     *            the expected url
-     */
-    public BaseSeleniumPage(final WebDriver webDriver, final String expectedUrl) {
-        super();
-        this.expectedUrl = expectedUrl;
-        this.webdriver = webDriver;
-    }
+	/**
+	 * Instantiates a new base selenium page.
+	 *
+	 * @param webDriver
+	 *            the web driver
+	 * @param expectedUrl
+	 *            the expected url
+	 */
+	public BaseSeleniumPage(final WebDriver webDriver, final String expectedUrl) {
+		super();
+		this.expectedUrl = expectedUrl;
+		this.webdriver = webDriver;
+	}
 
-    /**
-     * Gets the expected url.
-     *
-     * @return the expected url
-     */
-    public final String getExpectedUrl() {
-        return expectedUrl;
-    }
+	/**
+	 * Gets the expected url.
+	 *
+	 * @return the expected url
+	 */
+	public final String getExpectedUrl() {
+		return expectedUrl;
+	}
 
-    /**
-     * Click field.
-     *
-     * @param xpath
-     *            the xpath
-     */
-    protected final void clickField(final String xpath) {
-        this.findElementByXpath(xpath).click();
-    }
+	/**
+	 * Click field.
+	 *
+	 * @param xpath
+	 *            the xpath
+	 */
+	protected final void clickField(final String xpath) {
+		this.findElementByXpath(xpath).click();
+	}
 
-    /**
-     * Enter value into field.
-     *
-     * @param answer
-     *            the answer
-     * @param xpath
-     *            the xpath
-     * @return the web element
-     */
-    protected final WebElement enterValueIntoField(
-            final String answer,
-            final String xpath) {
-        final WebElement element = this.findElementByXpath(xpath);
-        element.clear();
-        element.sendKeys(answer);
-        return element;
+	/**
+	 * Enter value into field.
+	 *
+	 * @param answer
+	 *            the answer
+	 * @param xpath
+	 *            the xpath
+	 * @return the web element
+	 */
+	protected final WebElement enterValueIntoField(final String answer, final String xpath) {
+		final WebElement element = this.findElementByXpath(xpath);
+		element.clear();
+		element.sendKeys(answer);
+		return element;
 
-    }
+	}
 
-    /**
-     * Find element by xpath.
-     *
-     * @param xpath
-     *            the xpath
-     * @return the web element
-     */
-    protected final WebElement findElementByXpath(final String xpath) {
-        final WebElement findElement =
-                this.webdriver.findElement(By.xpath(xpath));
-        if (null == findElement) {
-            throw new NoSuchElementException("Could not find xpath " + xpath);
-        }
-        return findElement;
+	/**
+	 * Find element by xpath.
+	 *
+	 * @param xpath
+	 *            the xpath
+	 * @return the web element
+	 */
+	protected final WebElement findElementByXpath(final String xpath) {
+		final WebElement findElement = this.webdriver.findElement(By.xpath(xpath));
+		if (null == findElement) {
+			throw new NoSuchElementException("Could not find xpath " + xpath);
+		}
+		return findElement;
 
-    }
+	}
 
-    /**
-     * Gets the web driver.
-     *
-     * @return the web driver
-     */
-    public final WebDriver getWebDriver() {
-        return this.webdriver;
-    }
+	/**
+	 * Gets the web driver.
+	 *
+	 * @return the web driver
+	 */
+	public final WebDriver getWebDriver() {
+		return this.webdriver;
+	}
 
-    /**
-     * Keep number only.
-     *
-     * @param password2
-     *            the password2
-     * @return the string
-     */
-    protected final int keepNumberOnly(final String password2) {
-        return Integer.valueOf(password2.replaceAll("\\D+", ""));
-    }
+	/**
+	 * Keep number only.
+	 *
+	 * @param password2
+	 *            the password2
+	 * @return the string
+	 */
+	protected final int keepNumberOnly(final String password2) {
+		return Integer.valueOf(password2.replaceAll("\\D+", ""));
+	}
 
-    public boolean isElementPresent(By by) {
-        try {
-            this.getWebDriver().findElement(by);
-            return true;
-        }
-        catch (NoSuchElementException e) {
-            return false;
-        }
-    }
+	public boolean isElementPresent(By by) {
+		try {
+			this.getWebDriver().findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 
-    public boolean isAlertPresent() {
-        try {
-            this.getWebDriver().switchTo().alert();
-            return true;
-        }
-        catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+	public boolean isAlertPresent() {
+		try {
+			this.getWebDriver().switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
 
-    public String closeAlertAndGetItsText() {
-        try {
-            Alert alert = this.getWebDriver().switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            }
-            else {
-                alert.dismiss();
-            }
-            return alertText;
-        }
-        finally {
-            acceptNextAlert = true;
-        }
-    }
+	public String closeAlertAndGetItsText() {
+		try {
+			Alert alert = this.getWebDriver().switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
 
-    /**
-     * Wait for page to load.
-     */
-    protected final void waitForPageToLoad() {
-        try {
-            final int halfASecond = 500;
-            Thread.sleep(10 * halfASecond);
-            if(!SeleniumUtils.isInternetAvailable()){
-            	LOGGER.warn("no internet");
-            	waitForPageToLoad();
-            }
-        }
-        catch (final InterruptedException e) {
-            LOGGER.info("Interrupted");
-        }
-    }
+	/**
+	 * Wait for page to load.
+	 */
+	public final void waitForPageToLoad() {
+		WebDriver webDriver2 = getWebDriver();
+		waitForPageToLoad(webDriver2);
+	}
 
-    @Override
-    protected final void isLoaded() {
-        if (!this.getWebDriver().getCurrentUrl().equals(this.expectedUrl)) {
-            this.load();
-        }
-        String url = this.getWebDriver().getCurrentUrl();
-        if (!url.startsWith(this.expectedUrl)) {
-            throw new RuntimeException(this.expectedUrl
-                                       + " is  not loaded. Instead is " + url);
-        }
+	public static void waitForPageToLoad(WebDriver webDriver2) {
+		try {
+			final int halfASecond = 500;
+			Thread.sleep(10 * halfASecond);
+			if (!SeleniumUtils.isInternetAvailable()) {
+				LOGGER.warn("no internet - can't load " + webDriver2.getCurrentUrl());
+				waitForPageToLoad(webDriver2);
+				webDriver2.get(webDriver2.getCurrentUrl());
+			}
+		} catch (final InterruptedException e) {
+			LOGGER.info("Interrupted");
+		}
+	}
 
-    }
+	@Override
+	protected final void isLoaded() {
+		if (!this.getWebDriver().getCurrentUrl().equals(this.expectedUrl)) {
+			this.load();
+		}
+		String url = this.getWebDriver().getCurrentUrl();
+		if (!url.startsWith(this.expectedUrl)) {
+			throw new RuntimeException(this.expectedUrl + " is  not loaded. Instead is " + url);
+		}
+
+	}
 }
